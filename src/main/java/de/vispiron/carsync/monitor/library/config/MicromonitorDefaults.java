3714ae4996f137e6eeb3c9a80320a287b7fb2c3a
mehrdad1 <mehrdadallahkarami@gmail.com>
2019-01-09 11:41:27 +0100

    me

--- src/main/java/de/vispiron/carsync/monitor/library/config/MicromonitorDefaults.java
+++ src/main/java/de/vispiron/carsync/monitor/library/config/MicromonitorDefaults.java
@@ -4,118 +4,55 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-
-public interface MicromonitorDefaults {
-
-    interface Async {
-
-        int corePoolSize = 2;
-        int maxPoolSize = 50;
-        int queueCapacity = 10000;
+public interface MicroMonitorDefaults {
+    public interface Registry {
+        String password = null;
     }
 
-    interface Http {
-
-        MicroMonitorProperties.Http.Version version = MicroMonitorProperties.Http.Version.V_1_1;
-
-        boolean useUndertowUserCipherSuitesOrder = true;
-
-        interface Cache {
-
-            int timeToLiveInDays = 1461; // 4 years (including leap day)
-        }
+    public interface Ribbon {
+        String[] displayOnActiveProfiles = null;
     }
 
-    interface Cache {
+    public interface Gateway {
+        Map<String, List<String>> authorizedMicroservicesEndpoints = new LinkedHashMap();
 
-        interface Hazelcast {
-
-            int timeToLiveSeconds = 3600; // 1 hour
-            int backupCount = 1;
-
-            interface ManagementCenter {
-
-                boolean enabled = false;
-                int updateInterval = 3;
-                String url = "";
-            }
-        }
-
-        interface Ehcache {
-
-            int timeToLiveSeconds = 3600; // 1 hour
-            long maxEntries = 100;
-        }
-
-        interface Infinispan {
-
-            String configFile = "default-configs/default-jgroups-tcp.xml";
-            boolean statsEnabled = false;
-
-            interface Local {
-
-                long timeToLiveSeconds = 60; // 1 minute
-                long maxEntries = 100;
-            }
-
-            interface Distributed {
-
-                long timeToLiveSeconds = 60; // 1 minute
-                long maxEntries = 100;
-                int instanceCount = 1;
-            }
-
-            interface Replicated {
-
-                long timeToLiveSeconds = 60; // 1 minute
-                long maxEntries = 100;
-            }
-        }
-
-        interface Memcached {
-
+        public interface RateLimiting {
             boolean enabled = false;
-            String servers = "localhost:11211";
-            int expiration = 300; // 5 minutes
-            boolean useBinaryProtocol = true;
+            long limit = 100000L;
+            int durationInSeconds = 3600;
         }
     }
 
-    interface Mail {
-        boolean enabled = false;
-        String from = "";
-        String baseUrl = "";
+    public interface Social {
+        String redirectAfterSignIn = "/#/home";
     }
 
-    interface Security {
-
-        interface ClientAuthorization {
-
-            String accessTokenUri = null;
-            String tokenServiceId = null;
-            String clientId = null;
-            String clientSecret = null;
-        }
-
-        interface Authentication {
-
-            interface Jwt {
-
-                String secret = null;
-                String base64Secret = null;
-                long tokenValidityInSeconds = 1800; // 0.5 hour
-                long tokenValidityInSecondsForRememberMe = 2592000; // 30 hours;
-            }
-        }
-
-        interface RememberMe {
-
-            String key = null;
+    public interface Logging {
+        public interface Logstash {
+            boolean enabled = false;
+            String host = "localhost";
+            int port = 5000;
+            int queueSize = 512;
         }
     }
 
-    interface Swagger {
+    public interface Metrics {
+        public interface Prometheus {
+            boolean enabled = false;
+            String endpoint = "/prometheusMetrics";
+        }
 
+        public interface Logs {
+            boolean enabled = false;
+            long reportFrequency = 60L;
+        }
+
+        public interface Jmx {
+            boolean enabled = true;
+        }
+    }
+
+    public interface Swagger {
         String title = "Application API";
         String description = "API documentation";
         String version = "0.0.1";
@@ -127,67 +64,96 @@ public interface MicromonitorDefaults {
         String licenseUrl = null;
         String defaultIncludePattern = "/api/.*";
         String host = null;
-        String[] protocols = {};
+        String[] protocols = new String[0];
         boolean useDefaultResponseMessages = true;
     }
 
-    interface Metrics {
-
-        interface Jmx {
-
-            boolean enabled = true;
+    public interface Security {
+        public interface RememberMe {
+            String key = null;
         }
 
-        interface Logs {
+        public interface Authentication {
+            public interface Jwt {
+                String secret = null;
+                String base64Secret = null;
+                long tokenValidityInSeconds = 1800L;
+                long tokenValidityInSecondsForRememberMe = 2592000L;
+            }
+        }
 
+        public interface ClientAuthorization {
+            String accessTokenUri = null;
+            String tokenServiceId = null;
+            String clientId = null;
+            String clientSecret = null;
+        }
+    }
+
+    public interface Mail {
+        boolean enabled = false;
+        String from = "";
+        String baseUrl = "";
+    }
+
+    public interface Cache {
+        public interface Memcached {
             boolean enabled = false;
-            long reportFrequency = 60;
-
+            String servers = "localhost:11211";
+            int expiration = 300;
+            boolean useBinaryProtocol = true;
         }
 
-        interface Prometheus {
+        public interface Infinispan {
+            String configFile = "default-configs/default-jgroups-tcp.xml";
+            boolean statsEnabled = false;
 
-            boolean enabled = false;
-            String endpoint = "/prometheusMetrics";
+            public interface Replicated {
+                long timeToLiveSeconds = 60L;
+                long maxEntries = 100L;
+            }
+
+            public interface Distributed {
+                long timeToLiveSeconds = 60L;
+                long maxEntries = 100L;
+                int instanceCount = 1;
+            }
+
+            public interface Local {
+                long timeToLiveSeconds = 60L;
+                long maxEntries = 100L;
+            }
+        }
+
+        public interface Ehcache {
+            int timeToLiveSeconds = 3600;
+            long maxEntries = 100L;
+        }
+
+        public interface Hazelcast {
+            int timeToLiveSeconds = 3600;
+            int backupCount = 1;
+
+            public interface ManagementCenter {
+                boolean enabled = false;
+                int updateInterval = 3;
+                String url = "";
+            }
         }
     }
 
-    interface Logging {
+    public interface Http {
+        MicroMonitorProperties.Http.Version version = MicroMonitorProperties.Http.Version.V_1_1;
+        boolean useUndertowUserCipherSuitesOrder = true;
 
-        interface Logstash {
-
-            boolean enabled = false;
-            String host = "localhost";
-            int port = 5000;
-            int queueSize = 512;
+        public interface Cache {
+            int timeToLiveInDays = 1461;
         }
     }
 
-    interface Social {
-
-        String redirectAfterSignIn = "/#/home";
-    }
-
-    interface Gateway {
-
-        Map<String, List<String>> authorizedMicroservicesEndpoints = new LinkedHashMap<>();
-
-        interface RateLimiting {
-
-            boolean enabled = false;
-            long limit = 100_000L;
-            int durationInSeconds = 3_600;
-
-        }
-    }
-
-    interface Ribbon {
-
-        String[] displayOnActiveProfiles = null;
-    }
-
-    interface Registry {
-
-        String password = null;
+    public interface Async {
+        int corePoolSize = 2;
+        int maxPoolSize = 50;
+        int queueCapacity = 10000;
     }
 }
